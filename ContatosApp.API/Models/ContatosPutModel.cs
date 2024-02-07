f4160d2ae392277a5f6d80144bc4acfe49ed9c25
Danielnm <dn644625@gmail.com>
2024-02-07 12:59:26 -0300

    first commit

---
+++ ContatosApp.API/Models/ContatosPutModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContatosApp.API.Models
+{
+    public class ContatosPutModel
+    {
+        [Required(ErrorMessage = "informe o o id do cliente desejado.")]
+        public Guid? id { get; set; }
+
+        [MinLength(8, ErrorMessage = "Informe no mínimo {1} caracteres.")]
+        [MaxLength(100, ErrorMessage = "Informe no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe o nome do contato.")]
+        public string? Nome { get; set; }
+
+        [EmailAddress(ErrorMessage = "Por favor, informe um endereço de email válido.")]
+        [Required(ErrorMessage = "Por favor, informe o email do contato.")]
+        public string? Email { get; set; }
+
+        [RegularExpression(@"\(\d{2}\)\s\d{5}-\d{4}",
+            ErrorMessage = "Por favor, informe um telefone no formato: '(99) 99999-9999'.")]
+        [Required(ErrorMessage = "Por favor, informe o telefone do contato.")]
+        public string? Telefone { get; set; }
+
+    }
+}
